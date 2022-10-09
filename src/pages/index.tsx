9243c55cd15c84f52432924d6a3e14dfe0805ff9
Kevin Stubblefield <kevin@stubblefield.io>
2022-10-09 15:30:07 -0500

    add temporary environment check for debugging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { MainLayout } from '../layouts/main.layout';
 const Home: NextPage = () => {
   return (
     <MainLayout title="Home" description="User dashboard">
+      <p>Environment: {process.env.NODE_ENV}</p>
       <p>Hi. This will maybe be a dashboard? or just go to /households, idk</p>
     </MainLayout>
   );
