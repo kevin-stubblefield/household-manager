5b3ebee149d26b874bd0ac658be2323e39c8dfb7
Kevin Stubblefield <kevin@stubblefield.io>
2022-10-16 14:53:26 -0500

    if no tasks, don't display table

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -19,33 +19,37 @@ const TasksPage: NextPage = () => {
       description="Tasks for households the user is a part of"
     >
       <AddTaskForm />
-      <table className="border-collapse border border-slate-500 w-full">
-        <thead>
-          <tr>
-            <th className={cellClass}>Task</th>
-            <th className={cellClass}>Household</th>
-            <th className={cellClass}>Assigned To</th>
-            <th className={cellClass}>Priority</th>
-            <th className={cellClass}>Due</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data.map((task) => (
-              <tr key={task.id}>
-                <td className={cellClass}>{task.name}</td>
-                <td className={cellClass}>{task.household.name}</td>
-                <td className={cellClass}>
-                  {task.user?.displayName || task.user?.name || 'Not Assigned'}
-                </td>
-                <td className={cellClass}>{task.priority}</td>
-                <td className={cellClass}>
-                  {task.dueDate?.toLocaleDateString() || 'N/A'}
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      {data && data.length > 0 && (
+        <table className="border-collapse border border-slate-500 w-full">
+          <thead>
+            <tr>
+              <th className={cellClass}>Task</th>
+              <th className={cellClass}>Household</th>
+              <th className={cellClass}>Assigned To</th>
+              <th className={cellClass}>Priority</th>
+              <th className={cellClass}>Due</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.map((task) => (
+                <tr key={task.id}>
+                  <td className={cellClass}>{task.name}</td>
+                  <td className={cellClass}>{task.household.name}</td>
+                  <td className={cellClass}>
+                    {task.user?.displayName ||
+                      task.user?.name ||
+                      'Not Assigned'}
+                  </td>
+                  <td className={cellClass}>{task.priority}</td>
+                  <td className={cellClass}>
+                    {task.dueDate?.toLocaleDateString() || 'N/A'}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      )}
     </MainLayout>
   );
 };
