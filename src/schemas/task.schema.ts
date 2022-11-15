8d7490df0940ef3b4f5a5fc842ed50867d60d5a8
Kevin Stubblefield <kevin@stubblefield.io>
2022-11-15 17:52:16 -0600

    update createTaskSchema

--- src/schemas/task.schema.ts
+++ src/schemas/task.schema.ts
@@ -1,14 +1,26 @@
-import z from 'zod';
+import z, { string } from 'zod';
 
 export const createTaskSchema = z.object({
   name: z.string().min(2),
   householdId: z.string(),
+  isRecurring: z.boolean().optional().default(false),
+  recurrenceRule: z.string().optional(),
   frequency: z.string().optional().default('once'),
-  scheduledDay: z.number().optional(),
-  priority: z.number().optional().default(3),
+  interval: z.string().optional(),
+  startTime: z.date().optional(),
+  endTime: z.date().optional(),
+  byDate: z
+    .number()
+    .min(1)
+    .max(31)
+    .transform((val) => val.toString()),
+  byDay: z.string().min(2).max(3).optional(),
+  duration: z.string().optional(),
+  priority: z.number().min(1).max(3).optional().default(3),
   dueDate: z.date().optional(),
-  lastCompletedDate: z.date().optional(),
-  assignedTo: z.string().nullable(),
+  assignedTo: z.string().optional(),
+  setAsBusy: z.boolean().optional().default(false),
+  isAllDay: z.boolean().optional().default(false),
 });
 
 export type CreateTaskInput = z.TypeOf<typeof createTaskSchema>;
